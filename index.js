84a5d53f3ea8b4f675510216562018ab1d9a8b2e
Angel Uriel GR <uriel.gonzalezr@hotmail.com>
2026-08-18 12:06:41 -0600

    Agregado la operacion % y reduce decimales a 2

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function calculadora() {
     equalsButton.addEventListener('click', function() {
         const eval = evaluateExpression(textOperation);
         const reordered = reorderExpression(eval);
-        const result = resultExpression(reordered);
+        const result = Number(resultExpression(reordered)).toFixed(2);
         display.value = result;
         const newLi = document.createElement('LI');
         const operationText = textOperation + "=" + result;
@@ -73,6 +73,7 @@ function calculadora() {
                 return 1;
             case '*':
             case '/':
+            case '%':
                 return 2;
             default:
                 return 0;
@@ -141,6 +142,9 @@ function calculadora() {
                     }
                     result = b / a;
                     break;
+                case '%':
+                    result = b % a;
+                    break;
             }
             stack.push(result);
         }
@@ -156,93 +160,4 @@ function calculadora() {
         textOperation = textOperation.slice(0, -1);
         display.value = textOperation;
     });
-
-    // numberButton.forEach(button => {
-    //     button.addEventListener('click', function() {
-    //         currentValue += button.textContent;
-    //         display.value = currentValue;
-    //         if(previousValue) {
-    //             display.value = '';
-    //             display.value += previousValue;
-    //             display.value += operator;
-    //             display.value += currentValue;
-    //         }
-    //     })   
-    // });
-
-    // operatorButton.forEach(button => {
-    //     button.addEventListener('click', function() {
-    //         const selectedOperator = button.textContent;
-    //         if(selectedOperator === '-' && currentValue === '') {
-    //             currentValue = '-';
-    //             display.value = currentValue;
-    //             return;
-    //         }
-    //         if(currentValue) {
-    //             previousValue = currentValue;
-    //             currentValue = '';
-    //             operator = button.textContent;
-    //             display.value += operator;
-    //         }
-    //     })
-    // });
-
-    // equalsButton.addEventListener('click', function() {
-    //     if (!previousValue || !currentValue || !operator) {
-    //         return;
-    //     }
-
-    //     const num1 = Number(previousValue);
-    //     const num2 = Number(currentValue);
-
-    //     switch(operator) {
-    //         case '+':
-    //             result = num1 + num2;
-    //             break;
-    //         case '-':
-    //             result = num1 - num2;
-    //             break;
-    //         case '*':
-    //             result = num1 * num2;
-    //             break;
-    //         case '/':
-    //             if(num2 === 0) {
-    //                 result = 'No se puede dividir';
-    //                 break;
-    //             }
-    //             result = num1 / num2;
-    //             break;
-    //     }
-
-    //     display.value = Number(result).toFixed(2);
-    //     const newLi = document.createElement('LI');
-    //     const operationText = previousValue + operator + currentValue + "=" + Number(result).toFixed(2);
-    //     newLi.textContent = operationText;
-    //     historyList.prepend(newLi);
-    //     currentValue = result;
-    //     previousValue = '';
-    //     operator = null;
-    // })
-
-    // pointButton.addEventListener('click', function() {
-    //     if(!currentValue.includes('.')) {
-    //         currentValue += '.';
-    //         display.value = currentValue;
-    //     }
-    //     if(previousValue) {
-    //         display.value = previousValue + operator + currentValue;
-    //     }
-
-    // })
-
-    // parenthesisButton.addEventListener('click', function() {
-    //     if(currentValue.includes('(') && !currentValue.includes(')')) {
-    //         currentValue += ')';
-    //         display.value = currentValue;
-    //     }
-    //     if(!currentValue.includes('(')) {
-    //         currentValue += '(';
-    //         display.value = currentValue;
-    //     }
-    // })
 }
